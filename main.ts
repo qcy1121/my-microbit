910725948ab7be6b7c2357b08efc69d7f35db422
null <qcy1121@gmail.com>
2025-04-08 23:17:28 +0800

    优化代码

--- main.ts
+++ main.ts
@@ -1,37 +1,43 @@
 let 距离 = 0
 let 有人 = 0
-let 暗度 = 0
 let 黑天 = 0
-// 触摸P2播放音乐
+let 暗度 = 0
+serial.redirectToUSB()
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P2)) {
-        music.play(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
-    }
-})
-// 光线控制P16引脚
-basic.forever(function () {
-    暗度 = pins.analogReadPin(AnalogPin.P1)
-    if (暗度 >= 700) {
+    暗度 = pins.analogReadPin(AnalogReadWritePin.P3)
+    serial.writeValue("x", 暗度)
+    if (暗度 >= 600) {
         黑天 = 1
     } else {
         黑天 = 0
     }
-    pins.digitalWritePin(DigitalPin.P16, 黑天)
-})
-// 人体检测与超声波测距
-basic.forever(function () {
     有人 = pins.digitalReadPin(DigitalPin.P14)
-    basic.showNumber(有人)
+    serial.writeValue("y", 有人)
     if (有人 == 1) {
+        if (黑天 == 1) {
+            pins.digitalWritePin(DigitalPin.P16, 1)
+        } else {
+            pins.digitalWritePin(DigitalPin.P16, 0)
+        }
         距离 = sonar.ping(
         DigitalPin.P11,
         DigitalPin.P12,
         PingUnit.Centimeters
         )
-        if (距离 > 0 && 距离 < 20) {
-            music.play(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.UntilDone)
+        serial.writeValue("z", 距离)
+        if (距离 < 20) {
+            music.play(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
         }
-        // 避免频繁触发
-        basic.pause(500)
+    } else {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
+    // 避免频繁触发
+    basic.pause(500)
+})
+// 触摸P2播放音乐
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        music.play(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
     }
 })
